45f7d1ca40f2829a62dd836fba1f3261ecb86c61
Mariamcm90 <mariamcm90@gmail.com>
2018-03-25 11:42:19 -0400

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,34 @@
+chrome.runtime.onMessage.addListener(function(response, sender, sendResponse) {
+    alert(response);
+    var sampleItem = response;
+    var category1 = "plastic", category2 = "foam", category3 = "paper", category4 = "coffee";
+
+    if(sampleItem.contains(category1)){
+        change1 = "test";
+        change2 = "test1";
+        change3 = "test2";
+        change4 = "test3";
+    } else if (sampleItem.contains(category2)) {
+        change1 = "test";
+        change2 = "test1";
+        change3 = "test2";
+        change4 = "test3";
+    } else if (sampleItem.contains(category3) ) {
+        change1 = "test";
+        change2 = "test1";
+        change3 = "test2";
+        change4 = "test3";
+    } else {
+        change1 = "test";
+        change2 = "test1";
+        change3 = "test2";
+        change4 = "test3";
+    }
+
+    document.getElementById("water").innerHTML = change1;
+    document.getElementById("air").innerHTML = change2;
+    document.getElementById("labor").innerHTML = change3;
+    document.getElementById("transparency").innerHTML = change4;
+
+});
+
