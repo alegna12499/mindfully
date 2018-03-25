85c89aa8d664cab349e9450fa73c18bf599fc5ff
alegna12499 <alegna12499@gmail.com>
2018-03-25 12:36:58 -0400

    Update popup.js

--- popup.js
+++ popup.js
@@ -5,29 +5,33 @@ chrome.runtime.onMessage.addListener(function(response, sender, sendResponse) {
     if(sampleItem.contains(category1)){
         change1 = ".55 gallons of water / plate";
         change2 = "Can't be recycled :-(";
-        change3 = "test2";
-        change4 = "test3";
+        change3 = "Biogrades in composting facility in 12 weeks, Toxic chemicals: Dioxins and Polystyrene";
+        change4 = "Unknown contact https://www.gp.com/about-us/media-contacts";
+        change5 = "http://www.shopfoodex.com/chinet-classic-white-dinner-plates-count-p-27448.html";
     } else if (sampleItem.contains(category2)) {
         change1 = "test";
         change2 = "test1";
         change3 = "test2";
         change4 = "test3";
+        change5 = "test4";
     } else if (sampleItem.contains(category3) ) {
         change1 = "test";
         change2 = "test1";
         change3 = "test2";
         change4 = "test3";
+        change5 = "test4";
     } else {
         change1 = "test";
         change2 = "test1";
         change3 = "test2";
         change4 = "test3";
+        change5 = "test4";
     }
 
     document.getElementById("water").innerHTML = change1;
-    document.getElementById("environmental").innerHTML = change2;
-    document.getElementById("labor").innerHTML = change3;
+    document.getElementById("recycle").innerHTML = change2;
+    document.getElementById("environmental").innerHTML = change3;
     document.getElementById("transparency").innerHTML = change4;
-     document.getElementById("").innerHTML = change5;
+    document.getElementById("alternatives").innerHTML = change5;
 });
 
